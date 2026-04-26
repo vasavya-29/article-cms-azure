e0ab0e2e621be52ee4600dcb21f237b69c509e60
vasavya-29 <20ht1a4608@gmail.com>
2026-04-26 14:14:55 -0500

    Fix password encoding using quote_plus

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 ﻿import os
+from urllib.parse import quote_plus
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -7,8 +8,18 @@ class Config(object):
     SQL_DATABASE  = os.environ.get('SQL_DATABASE')
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
     SQL_PASSWORD  = os.environ.get('SQL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    _pwd = quote_plus(os.environ.get('SQL_PASSWORD') or '')
+    _user = os.environ.get('SQL_USER_NAME') or ''
+    _server = os.environ.get('SQL_SERVER') or ''
+    _db = os.environ.get('SQL_DATABASE') or ''
+
+    SQLALCHEMY_DATABASE_URI = (
+        f'mssql+pyodbc://{_user}:{_pwd}@{_server}:1433/{_db}'
+        '?driver=ODBC+Driver+17+for+SQL+Server'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     BLOB_ACCOUNT     = os.environ.get('BLOB_ACCOUNT')
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
     BLOB_CONTAINER   = os.environ.get('BLOB_CONTAINER')
