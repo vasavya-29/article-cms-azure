ae0d2f10131a72d4d2afdbc1a0d62dea64772b39
vasavya-29 <20ht1a4608@gmail.com>
2026-04-26 19:50:11 -0500

    Add URL.create to __init__.py v2

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,22 +1,17 @@
-﻿"""
-The flask application package.
-"""
+﻿"""The flask application package."""
 import logging
 import os
-import pyodbc
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 from flask.logging import create_logger
-from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Override DB connection using explicit pyodbc params
 connection_url = URL.create(
     'mssql+pyodbc',
     username=os.environ.get('SQL_USER_NAME'),
